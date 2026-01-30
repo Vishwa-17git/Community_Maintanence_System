fae75dbf72a6d172cbef932af46e848139bdc740
Your Name <youremail@example.com>
2026-01-31 00:36:57 +0530

    Fix router white screen issue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
-import ReportIssue from "./pages/ReportIssue";
-import IssueDetails from "./pages/IssueDetails";
-import Volunteers from "./pages/Volunteers";
-import Rewards from "./pages/Rewards";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/report" element={<ReportIssue />} />
-        <Route path="/issue/:id" element={<IssueDetails />} />
-        <Route path="/volunteers" element={<Volunteers />} />
-        <Route path="/rewards" element={<Rewards />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Community Maintenance System</h1>} />
+    </Routes>
   );
 }
 
